47596325adf46f17ab4df573c580063b541819fb
alordiel <alexander.vasilev@protonmail.com>
2024-08-19 11:10:21 +0300

    initial code

---
+++ open-all-post-links-in-new-tab.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Open All Post Links In New Tab
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Alexander Vasilev
+ * Author URI:      https://timelinedev.com
+ * Text Domain:     open-all-post-links-in-new-tab
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Open_All_Post_Links_In_New_Tab
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Open_All_Post_Links_In_New_Tab
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
